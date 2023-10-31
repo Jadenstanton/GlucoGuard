bc59b796e5979df51aa132154b041dd359c00230
Jaden <jaden.stanton6@gmail.com>
2023-10-31 15:17:41 -0500

    modified conftest.py to handle setup_user

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,26 +3,40 @@ from ..app import create_app
 from config import TestingConfig
 from backend.database.database import db
 from sqlalchemy.orm import sessionmaker
+from backend.api.models.user import UserProfile
 
 
 @pytest.fixture(scope="session")
-def test_app():
+def app():
     """Create and configure a new app instance for testing."""
     app = create_app(TestingConfig)
     with app.app_context():
         yield app  # this is the app instance you'll use for testing
 
 
+# New Fixture: Setup a user for tests
+@pytest.fixture(scope="function")
+def setup_user(db_session):
+    user = UserProfile(
+        username="testuser",
+        email="testuser@example.com",
+        password="password123",  # TODO: Hash this password
+    )
+    db_session.add(user)
+    db_session.commit()
+    yield user
+
+
 @pytest.fixture(scope="session")
-def test_client(test_app):
+def test_client(app):
     """A test client for the app."""
-    return test_app.test_client()
+    return app.test_client()
 
 
 @pytest.fixture(scope="session")
-def test_db(test_app):
+def test_db(app):
     """Session-wide test database."""
-    with test_app.app_context():
+    with app.app_context():
         db.create_all()
         yield db  # this is the database session you'll use for testing
         db.drop_all()
