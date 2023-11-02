0d49b82dcd7fefa6bcd7e999f246a1ac0dc0d2ce
Jaden <jaden.stanton6@gmail.com>
2023-11-01 21:40:33 -0500

    Updated food routes, controller, and model to handle Edamam api integration

--- backend/api/controllers/food_controller.py
+++ backend/api/controllers/food_controller.py
@@ -1,4 +1,6 @@
 from ..models.food import Food
+import logging
+import json
 
 
 class FoodController:
@@ -9,23 +11,69 @@ class FoodController:
         if not data:
             return {"message": "No data provided in the request"}, 400
 
+        print(json.dumps(data, indent=4))
+
         if "food_name" not in data or "portion_size" not in data:
             return {"message": "Food name and portion size are required fields"}, 400
 
-        food = Food(
+        if data["portion_size"] == "Not Defined":
+            data["portion_size"] = 0
+
+        def get_nutrient_value(key):
+            total_nutrients_data = data.get("nutritional_info", {}).get(
+                "totalNutrients", {}
+            )
+            if key in total_nutrients_data:
+                return total_nutrients_data[key].get("quantity", None)
+
+            return data.get("nutritional_info", {}).get(key, None)
+
+        calories = get_nutrient_value("calories")
+        total_fat_g = get_nutrient_value("FAT")
+        saturated_fat_g = get_nutrient_value("FASAT")
+        trans_fat_g = get_nutrient_value("FATRN")
+        cholesterol_mg = get_nutrient_value("CHOLE")
+        sodium_mg = get_nutrient_value("NA")
+        total_carbohydrate_g = get_nutrient_value("CHOCDF")
+        dietary_fiber_g = get_nutrient_value("FIBTG")
+        total_sugars_g = get_nutrient_value("SUGAR")
+        protein_g = get_nutrient_value("PROCNT")
+        vitamin_d_ug = get_nutrient_value("VITD")
+        calcium_mg = get_nutrient_value("CA")
+        iron_mg = get_nutrient_value("FE")
+        potassium_mg = get_nutrient_value("K")
+
+        # Create a new Food entry
+        new_food_entry = Food(
             user_id=user_id,
             food_name=data["food_name"],
             portion_size=data["portion_size"],
-            nutritional_info=data.get("nutritional_info"),
+            calories=calories,
+            total_fat_g=total_fat_g,
+            saturated_fat_g=saturated_fat_g,
+            trans_fat_g=trans_fat_g,
+            cholesterol_mg=cholesterol_mg,
+            sodium_mg=sodium_mg,
+            total_carbohydrate_g=total_carbohydrate_g,
+            dietary_fiber_g=dietary_fiber_g,
+            total_sugars_g=total_sugars_g,
+            protein_g=protein_g,
+            vitamin_d_ug=vitamin_d_ug,
+            calcium_mg=calcium_mg,
+            iron_mg=iron_mg,
+            potassium_mg=potassium_mg,
         )
-
+        logging.debug("Attempting to add food entry to the database.")
         try:
-            self.db.session.add(food)
+            self.db.session.add(new_food_entry)
             self.db.session.commit()
-            return {"message": "Food entry created successfully", "data": food.id}, 201
+            return {
+                "message": "Food entry created successfully",
+                "data": new_food_entry.id,
+            }, 201
         except Exception as e:
             self.db.session.rollback()
-            return {"message": "Failed to create food entry"}, 500
+            return {"message": f"Failed to create food entry. Error:{e}"}, 500
 
     def delete_food(self, user_id, food_id):
         if not food_id:
@@ -54,9 +102,6 @@ class FoodController:
         if not data:
             return {"message": "No data provided in the request"}, 400
 
-        if "food_name" not in data:
-            return {"message": "Food name is a required field"}, 400
-
         food = Food.query.get(food_id)
 
         if not food:
@@ -67,11 +112,32 @@ class FoodController:
 
         food.food_name = data["food_name"]
         food.portion_size = data.get("portion_size")
-        food.nutritional_info = data.get("nutritional_info")
+        nutritional_info = data.get("nutritional_info", {})
 
+        for key, value in nutritional_info.items():
+            if hasattr(food, key):  # Check if attribute exists before setting it
+                setattr(food, key, value)
         try:
             self.db.session.commit()
             return {"message": "Food entry updated successfully", "data": food.id}
         except Exception as e:
             self.db.session.rollback()
             return {"message": "Failed to update food entry"}, 500
+
+    def get_food(self, user_id, food_id):
+        if not food_id:
+            return {"message": "Food ID is required in the request parameters"}, 400
+
+        food = Food.query.get(food_id)
+
+        if not food:
+            return {"message": "Food entry not found"}, 404
+
+        if food.user_id != user_id:
+            return {"message": "Permission denied"}, 403
+
+        response = {
+            "message": "Food entry retrieved successfully",
+            "data": food.to_dict(),
+        }
+        return response, 200
--- backend/api/models/food.py
+++ backend/api/models/food.py
@@ -3,8 +3,6 @@ from ...database.database import db
 
 
 class Food(db.Model):
-    # __bind_key__ = "user_data"  # Associate the model with the user_data database
-
     __tablename__ = "foods"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -15,11 +13,83 @@ class Food(db.Model):
     )
     food_name = db.Column(db.String(100), nullable=False)
     portion_size = db.Column(db.Float, nullable=False)
-    nutritional_info = db.Column(db.JSON)
-    # Add other fields as needed
 
-    def __init__(self, user_id, food_name, portion_size, nutritional_info):
+    # Nutritional Information
+    calories = db.Column(db.Float)
+    total_fat_g = db.Column(db.Float)
+    saturated_fat_g = db.Column(db.Float)
+    trans_fat_g = db.Column(db.Float)
+    cholesterol_mg = db.Column(db.Float)
+    sodium_mg = db.Column(db.Float)
+    total_carbohydrate_g = db.Column(db.Float)
+    dietary_fiber_g = db.Column(db.Float)
+    total_sugars_g = db.Column(db.Float)
+    protein_g = db.Column(db.Float)
+    vitamin_d_ug = db.Column(db.Float)
+    calcium_mg = db.Column(db.Float)
+    iron_mg = db.Column(db.Float)
+    potassium_mg = db.Column(db.Float)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "food_name": self.food_name,
+            "portion_size": self.portion_size,
+            "calories": self.calories,
+            "total_fat_g": self.total_fat_g,
+            "saturated_fat_g": self.saturated_fat_g,
+            "trans_fat_g": self.trans_fat_g,
+            "cholesterol_mg": self.cholesterol_mg,
+            "sodium_mg": self.sodium_mg,
+            "total_carbohydrate_g": self.total_carbohydrate_g,
+            "dietary_fiber_g": self.dietary_fiber_g,
+            "total_sugars_g": self.total_sugars_g,
+            "protein_g": self.protein_g,
+            "vitamin_d_ug": self.vitamin_d_ug,
+            "calcium_mg": self.calcium_mg,
+            "iron_mg": self.iron_mg,
+            "potassium_mg": self.potassium_mg,
+        }
+
+    # Constructor
+    def __init__(self, user_id, food_name, portion_size, **kwargs):
         self.user_id = user_id
         self.food_name = food_name
         self.portion_size = portion_size
-        self.nutritional_info = nutritional_info
+        self.calories = kwargs.get("calories")
+        self.total_fat_g = kwargs.get("total_fat_g")
+        self.saturated_fat_g = kwargs.get("saturated_fat_g")
+        self.trans_fat_g = kwargs.get("trans_fat_g")
+        self.cholesterol_mg = kwargs.get("cholesterol_mg")
+        self.sodium_mg = kwargs.get("sodium_mg")
+        self.total_carbohydrate_g = kwargs.get("total_carbohydrate_g")
+        self.dietary_fiber_g = kwargs.get("dietary_fiber_g")
+        self.total_sugars_g = kwargs.get("total_sugars_g")
+        self.protein_g = kwargs.get("protein_g")
+        self.vitamin_d_ug = kwargs.get("vitamin_d_ug")
+        self.calcium_mg = kwargs.get("calcium_mg")
+        self.iron_mg = kwargs.get("iron_mg")
+        self.potassium_mg = kwargs.get("potassium_mg")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "food_name": self.food_name,
+            "portion_size": self.portion_size,
+            "calories": self.calories,
+            "total_fat_g": self.total_fat_g,
+            "saturated_fat_g": self.saturated_fat_g,
+            "trans_fat_g": self.trans_fat_g,
+            "cholesterol_mg": self.cholesterol_mg,
+            "sodium_mg": self.sodium_mg,
+            "total_carbohydrate_g": self.total_carbohydrate_g,
+            "dietary_fiber_g": self.dietary_fiber_g,
+            "total_sugars_g": self.total_sugars_g,
+            "protein_g": self.protein_g,
+            "vitamin_d_ug": self.vitamin_d_ug,
+            "calcium_mg": self.calcium_mg,
+            "iron_mg": self.iron_mg,
+            "potassium_mg": self.potassium_mg,
+        }
