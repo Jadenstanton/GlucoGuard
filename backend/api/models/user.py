45ef970e908ac2cff6f70c7c80e1ca0b715e3777
Jaden <jaden.stanton6@gmail.com>
2023-11-09 16:15:44 -0600

    Resolve merge conflict in user.py

--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -10,8 +10,7 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    # Add other fields as needed
+    password = db.Column(db.String, nullable=False)
 
     def __init__(self, username, email, password):
         self.username = username
