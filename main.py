4f9e4fc0bd4b856d9997d748836f86e4d8323bb4
aariii <85893087+aariii@users.noreply.github.com>
2023-11-16 12:07:07 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,81 @@
-# import xlsxwriter module
-import xlsxwriter
+import pandas as pd
+import random
+#import xlsxwriter
 
-# Workbook() takes one, non-optional, argument
-# which is the filename that we want to create.
-workbook = xlsxwriter.Workbook('hello.xlsx')
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns",None)
 
-# The workbook object is then used to add new
-# worksheet via the add_worksheet() method.
-worksheet = workbook.add_worksheet()
 
-# Use the worksheet object to write
-# data via the write() method.
-worksheet.write('A1', 12)
-worksheet.write('B1', 'Geeks')
-worksheet.write('C1', 'For')
-worksheet.write('D1', 'Geeks')
 
-# Finally, close the Excel file
-# via the close() method.
-workbook.close()
+def generate_data(num_entries):
+    data = []
+    for _ in range(num_entries):
+        data.append({
+            'ActivityLevel' : random.randint(0, 1200),
+            'BreathingRate' : random.uniform(8,20),
+            'HeartRate' :     random.randint(40,120)
+        })
+    return pd.DataFrame(data)
 
-#user data will be selected from a database
-userData = input("Activity Levels")
-userData_int = int(userData)
-"""recommend level of activity per month"""
+#Generate 200 entries of data
+df = generate_data(200)
+
+
+#Define thresholds
 ActiveZonePreventation = 600
 BreathingRatePreventation = 12
 HeartRatePreventation = 60
-#SELECT ACTIVITYLEVEL FROM USERDATA
-#Monthly Activity levels
-if userData_int > ActiveZonePreventation:
-    print("chance of getting diabetes is low")
-elif userData_int < ActiveZonePreventation:
-    print("chance of getting diabetes is high")
-else:
-    print("your activity level is equal to the recommended monthly levels")
 
-#Montly breathing rate levels
-userData = input("Breathing Rate levels")
-if userData_int > BreathingRatePreventation:
-    print("chance of getting diabetes is high")
-elif userData_int <= BreathingRatePreventation:
-    print("chance of getting diabetes is low")
-else:
-    print("your breathing level is equal to the recommended monthly levels")
+#Process each entry
+for index, row in df.iterrows():
+    activity_level = row['ActivityLevel']
+    breathing_rate = row['BreathingRate']
+    heart_rate = row['HeartRate']
 
-#Monhtly Heart Rate levels
-userData = input("Heart Rate levels")
-if userData_int > HeartRatePreventation:
-    print("chance of getting diabetes is low")
-elif userData_int <= HeartRatePreventation:
-    ("chance of getting diabetes is high")
-else:
-    print("your heart rate level is equal to the recommended monthly levels")
+   #Logic for activity level
+    if activity_level > ActiveZonePreventation:
+        df.at[index, 'ActivityRisk'] = 'Your chance of getting diabetes is low'
+    elif activity_level < ActiveZonePreventation:
+        df.at[index, 'ActivityRisk'] = 'Your chance of getting diabetes is high'
+    else:
+        df.at[index, 'ActivityRisk'] = 'Your activity levels are equal to the recommended monthly levels'
 
 
-    """Save information from CSV file into a dictionary.
-    Then call the dictionary when you need to use a value"""
+       
+    if activity_level > BreathingRatePreventation:
+        df.at[index, 'BreathingRisk'] = 'Your chance of getting diabetes is low'
+    elif activity_level < BreathingRatePreventation:
+        df.at[index, 'BreathingRisk'] = 'Your chance of getting diabetes is high'
+    else:
+        df.at[index, 'BreathingRisk'] = 'Your breathing levels are equal to the recommended monthly levels'
+
+
+        #Logic for heart rate
+    if activity_level > HeartRatePreventation:
+        df.at[index, 'HeartRisk'] = 'Your chance of getting diabetes is low'
+    elif activity_level < HeartRatePreventation:
+        df.at[index, 'HeartRisk'] = 'Your chance of getting diabetes is high'
+    else:
+        df.at[index, 'HeartRisk'] = 'Your heart rate levels are equal to the recommended monthly levels'
+
+
+
+
+    """# Workbook() takes one, non-optional, argument
+    # which is the filename that we want to create.
+    workbook = xlsxwriter.Workbook('Userdata.xlsx')
+
+    # The workbook object is then used to add new
+    # worksheet via the add_worksheet() method.
+    worksheet = workbook.add_worksheet()
+
+    # Use the worksheet object to write
+    # data via the write() method.
+    df.to_csv('Userdata.xlsx')
+
+    # Finally, close the Excel file
+    # via the close() method.
+    workbook.close()"""
+
+print(df)
+
